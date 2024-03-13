2608ad7462d4d3615c9e34f4a710a6e5cd2f52cb
inorf <dimond23177@gmail.com>
2024-03-13 17:42:10 +0100

    Create askingJS.js

---
+++ askingJS.js
@@ -0,0 +1,14 @@
+function podtw(){
+    if(document.getElementById("input_odp1").checked){
+        alert("radio 1 checked");
+    }
+    if(document.getElementById("input_odp2").checked){
+        alert("radio 2 checked");
+    }
+    if(document.getElementById("input_odp3").checked){
+        alert("radio 3 checked");
+    }
+    if(document.getElementById("input_odp4").checked){
+        alert("radio 4 checked");
+    }
+}
